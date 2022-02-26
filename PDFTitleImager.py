7d72dd0b460508c2a78448d915ce4ed0d5f6f160
bobNZ <rogerj.nz@gmail.com>
2022-02-26 20:43:33 +1300

    Updated Summary Output

--- PDFTitleImager.py
+++ PDFTitleImager.py
@@ -20,21 +20,27 @@ class PDFTitlePageImager(object):
             imageList = self.getFileList(dirName, "jpg")
             fileListCount = len(fileList)
             imageListCount = len(imageList)
-            
+            processed = 0
+
             if not fileList: # Just skip directories that are empty
-                self.print("EMPTY      DIRECTORY #" + str(dirListCount) + " " + dirName)
+                self.print("EMPTY           DIRECTORY #" + str(dirListCount) + " " + dirName)
             elif not force and ((fileList[fileListCount-1].replace("pdf","jpg") in imageList) 
                 and (fileListCount <= imageListCount)): # If last PDF has an image then assume all PDFs have an image. But double check that there are at least as many images as PDFs
-                self.print("SKIPPED    DIRECTORY #" + str(dirListCount) + " " + dirName)
+                self.print("SKIPPED         DIRECTORY #" + str(dirListCount) + " " + dirName)
             else:
-                self.print("PROCESSING DIRECTORY #" + str(dirListCount) + " " + dirName)
+                self.verbose_print("PROCESSING DIRECTORY #" + str(dirListCount) + " " + dirName)
+                self.summary_print("     PROCESSING DIRECTORY #" + str(dirListCount) + " " + dirName)
                 for fileName in fileList:
                     if fileName.replace("pdf","jpg") in imageList: # If there is an image for the PDF then it has already been processed
                         self.verbose_print(" Skipped   #" + str(fileListCount) + " " + os.path.basename(fileName) + " ALREADY PROCESSED")
                     else:
                         self.verbose_print(" Processed #" + str(fileListCount) + " " + os.path.basename(fileName))
                         self.createImage(fileName, quality)
+                        processed += 1
+                    self.summary_print(f"{fileListCount: >4d}")
                     fileListCount -= 1
+                if not self._verbose:
+                    self.print(f"{processed: >4d}-PROCESSED ")
             dirListCount -= 1
         
 
@@ -63,6 +69,10 @@ class PDFTitlePageImager(object):
     def verbose_print(self, *args):
         if (self._verbose): 
             print(*args)
+
+    def summary_print(self, *args):
+        if (not self._verbose): 
+            print(*args, end="\r", flush=True)
 
     def print(self, *args):
         print(*args)
